74d7be5349bc8e0babc958aef449588e02a362e3
Melikenur Alan <melikenur.alan@ogr.gelisim.edu.tr>
2025-09-25 21:11:23 +0300

    shared contracts added

---
+++ src/shared/Shared.Contracts/Events/Users/PasswordResetEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Contracts.Events.Users
+{
+    public record PasswordResetEvent
+    {
+        public string Email { get; init; }
+        public string ResetLink { get; init; }
+        public DateTime RequestedAt { get; init; } = DateTime.UtcNow;
+    }
+}
---
+++ src/shared/Shared.Contracts/Events/Users/UserCreatedEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Contracts.Events.Users
+{
+    public record UserCreatedEvent
+    {
+        public Guid UserId { get; init; }
+        public string Email { get; init; } = default!;
+        public string UserName { get; init; } = default!;
+        public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+    }
+}
