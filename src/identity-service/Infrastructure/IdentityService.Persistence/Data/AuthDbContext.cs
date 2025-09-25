c4d143df15ab3cc3a6c1d1570ffca27ae068aa58
Melikenur Alan <melikenur.alan@ogr.gelisim.edu.tr>
2025-09-25 20:53:33 +0300

    Merged remote and local changes

---
+++ src/identity-service/Infrastructure/IdentityService.Persistence/Data/AuthDbContext.cs
@@ -0,0 +1,35 @@
+﻿using IdentityService.Persistence.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityService.Persistence.Data
+{
+    public class AuthDbContext : IdentityDbContext<AppUser, AppRole, Guid>
+    {
+        public AuthDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+
+
+        protected override  void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+
+            builder.Entity<AppUser>(b =>
+            {
+                b.HasIndex(u => u.ReferralCode).IsUnique();
+            }); // todo : auto generated geri dönüp incele !!!!
+
+
+        }
+    }
+}
---
+++ src/identity-service/Infrastructure/IdentityService.Persistence/Identity/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityService.Persistence.Identity
+{
+    public class AppUser : IdentityUser<Guid>
+    {
+        public string? ReferralCode { get; set; }
+        public bool IsEmailVerified { get; set; }
+        public bool IsPhoneVerified { get; set; }
+
+      //  public TwoFactorType TwoFactorType { get; set; }
+    }
+}
